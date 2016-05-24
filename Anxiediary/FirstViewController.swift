6491a17ae1253b7afa87150ad97b58313769b11a
Samuel Jones <i7262665@mac34616.local>
2016-05-24 11:02:07 +0100

    added reset warning

--- Anxiediary/FirstViewController.swift
+++ Anxiediary/FirstViewController.swift
@@ -11,27 +11,36 @@ class FirstViewController: UIViewController {
     }
 
     @IBAction func reset(sender: AnyObject) {
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext
-        let fetchRequest = NSFetchRequest(entityName: "Post")
-        var posts = [Post]()
-        do {
-            let results = try managedContext.executeFetchRequest(fetchRequest)
-            posts = results as! [Post]
-        } catch let error as NSError {
-            print("Could not fetch \(error), \(error.userInfo)")
-        }
         
-        for post in posts {
-            managedContext.deleteObject(post)
-        }
+        let alert=UIAlertController(title: "Reset current progress?", message: "Once you clear all your days, you cannot get them back.", preferredStyle: UIAlertControllerStyle.ActionSheet);
+   
+        presentViewController(alert, animated: true, completion: nil)
         
-        do {
-            try managedContext.save()
-        } catch let error as NSError  {
-            print("Could not save \(error), \(error.userInfo)")
-        } 
-
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil));
+        //event handler with closure
+        alert.addAction(UIAlertAction(title: "Reset", style: .Destructive, handler: { _ in
+            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            let managedContext = appDelegate.managedObjectContext
+            let fetchRequest = NSFetchRequest(entityName: "Post")
+            var posts = [Post]()
+            do {
+                let results = try managedContext.executeFetchRequest(fetchRequest)
+                posts = results as! [Post]
+            } catch let error as NSError {
+                print("Could not fetch \(error), \(error.userInfo)")
+            }
+            
+            for post in posts {
+                managedContext.deleteObject(post)
+            }
+            
+            do {
+                try managedContext.save()
+            } catch let error as NSError  {
+                print("Could not save \(error), \(error.userInfo)")
+            }
+        }))
+        
     }
     
 }
